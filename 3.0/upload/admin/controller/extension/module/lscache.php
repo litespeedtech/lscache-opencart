4bb53c0b16f57993691351e45fae70aa8d777e44
WuhuaChen <34863158+WuhuaChen@users.noreply.github.com>
2023-01-05 05:07:53 -0500

    [Update] Fixed a bug about htaccess mobile bot setting

--- 3.0/upload/admin/controller/extension/module/lscache.php
+++ 3.0/upload/admin/controller/extension/module/lscache.php
@@ -561,7 +561,7 @@ class ControllerExtensionModuleLSCache extends Controller {
         $directives .= '## Uncomment the following directives only if enabled GUI Option of Separate Cache Copy for Mobile Device, "yoursite.domain" need to be changed' . PHP_EOL;
         $directives .= '##RewriteCond %{HTTP_USER_AGENT} "iPhone|iPod|BlackBerry|Palm|Googlebot-Mobile|Mobile|mobile|mobi|Windows Mobile|Safari Mobile|Android|Opera Mini" [NC] ' . PHP_EOL;
         $directives .= '##RewriteCond %{HTTP_USER_AGENT} Bot' . PHP_EOL;
-        $directives .= '##RewriteRule .* - [CO=_lscache_vary:device&3Amobile:.yoursite.domain:0:/]' . PHP_EOL;
+        $directives .= '##RewriteRule .* - [CO=_lscache_vary:device\%3Amobile:yoursite.domain]' . PHP_EOL;
         $directives .= '</IfModule>' . PHP_EOL;
         $directives .= '### LITESPEED_CACHE_END';
 
